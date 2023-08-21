c358bc68c7980f1aee0ea292b3f6e99e84c89b5a
divyjainjd <divy.jain@bombaysoftwares.com>
2023-08-21 12:26:40 +0530

    made changes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,58 @@
 import { MONTH } from "./constants";
 
 /**
-* Checks if the value provided is none of this - null,undefined,empty string,"undefined",empty array as string
-* @param {any} obj - The value to be checked.
-* @example
-* isSet("null"); returns false
-* @example
-* isSet("some value"); returns true
-* @return {Boolean} - true if the value is set, false otherwise.
-*/
-export const isSet = (obj: any): boolean => {
-    // check if the value is provided is any of the conditions.
-    if (obj && obj !== "null" && obj !== undefined && obj !== "" && obj !== "[]" && obj !== "undefined" && typeof obj !== "undefined") {
-        return true;
+ * Converts a number to a two-digit format by adding a leading zero if necessary.
+ * @param {number} data - The number to be formatted.
+ * @example 
+ * getTwodigitFormat(8); returns 08
+ * getTwodigitFormat(9); returns 09
+ * @example
+ * getTwodigitFormat(10); returns 10
+ * @returns {string | number} - The formatted number as a string if less than 10, otherwise the original number.
+ */
+export const getTwodigitFormat = (data: number): string | number | null=> {
+    // Check if the data is greater than 9
+    // If true, return the data as it is
+    // If false, add a leading zero to the data and return it as a string
+    if (data > 99) {
+        // Handle the case when the data is three digits
+        return null;
     }
-    return false;
+    return data > 9 ? data : "0" + data;
+};
+
+/**
+ * Converts a Unix timestamp to a string representing the date and time in the format: DD-MM-YYYY HH:mm.
+ * @param {number} timestamp - The Unix timestamp to be formatted.
+ * @example
+ * formatTimestamp(1687244413); returns 20-6-2023 07:00
+ * @returns {string} - The formated string representation of date and time.
+ */
+export const formatTimestamp = (timestamp: number): string => {
+    // Create Date objects for the current time and the specified timestamp
+    let currentTime = new Date();
+    let Time = new Date(timestamp * 1000);
+
+    // Extract date components
+    let date = getTwodigitFormat(Time.getDate());
+    let month = getTwodigitFormat(Time.getMonth() + 1);
+    let year = Time.getFullYear();
+
+    // Extract time components
+    let hours = getTwodigitFormat(Time.getHours());
+    let minutes = getTwodigitFormat(Time.getMinutes());
+
+    // Return the formatted string representation of date and time
+    return (
+        date +
+        '-' +
+        month +
+        '-' +
+        year +
+        ' ' +
+        hours +
+        ':' +
+        minutes +
+        ' '
+    );
 };
